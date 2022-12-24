527f3e611153e5052c608e1ba29e2466922d6562
den <freeforelectron2@gmail.com>
2022-12-24 14:17:35 +0300

    reader added

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import './ScanPage.css';
+
+import QrReader from "react-web-qr-reader";
+
 
 import useWindowDimensions from "../hooks/useWindowDimensions";
 
 const ScanPage = () => {
     const { height, width } = useWindowDimensions();
+
+
+    const delay = 500;
+
+    const previewStyle = {
+        height: {height},
+        width: {width}
+    };
+
+    const [result, setResult] = useState("No result");
+
+    const handleScan = (result) => {
+        if (result) {
+        console.log(result)
+
+        setResult( prev => result.data)
+        }
+    };
+
+    const handleError = (error) => {
+        console.log(error);
+    };
 
     const squareSize = `${(Number(Math.min(height, width)) - 30)}px`;
 
@@ -12,14 +37,22 @@ const ScanPage = () => {
         <div className="wrapper">
             <div className="scan-preview">
                 <div style={{width: `${squareSize}`, height: `${squareSize}`, backgroundColor: 'red'}}>
-                    {squareSize}
+                    <QrReader
+                        delay={delay}
+                        style={previewStyle}
+                        onError={handleError}
+                        onScan={handleScan}
+                    />
                 </div>
             </div>
             <div className="result-preview">
-                result
+                {result}
             </div>
         </div>
     )
 }
 
 export default ScanPage
+
+
+
